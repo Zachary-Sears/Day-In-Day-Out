3380439f7dc7e9f8b260c19ca447d752545a4315
Zachary-Sears <44513683+Zachary-Sears@users.noreply.github.com>
2018-10-26 21:30:57 -0500

    Happiness bar script implemented
    Will not work quite yet becase there is nothing in the game to effect the bar yet.

--- Day In, Day Out/Assets/Scripts/UI_Manager.cs
+++ Day In, Day Out/Assets/Scripts/UI_Manager.cs
@@ -1,8 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
-    
+    public Image currentHappiness;
+
+    private float happyMax;
+    private float maxHappiness;
+
+    private void Start()
+    {
+        currentHappiness = GetComponent<Image>();
+        UpdateHappiness();
+    }
+
+    private void UpdateHappiness()
+    {
+        float ratio = happyMax / maxHappiness;
+        currentHappiness.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void DecreaseHappiness(float decreaseHappiness)
+    {
+        UpdateHappiness();
+    }
+
+    private void IncreaseHappiness(float increaseHappiness)
+    {
+        UpdateHappiness();
+    }
 }
