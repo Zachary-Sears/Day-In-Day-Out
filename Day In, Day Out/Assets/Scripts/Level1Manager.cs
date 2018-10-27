0ad841f9396489553a13e6b74e37821e1403153a
WDTOD1 <44513754+WDTOD1@users.noreply.github.com>
2018-10-26 21:23:12 -0500

    LastUpdateFromMeforTonight

---
+++ Day In, Day Out/Assets/Scripts/Level1Manager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour {
+
+    float timeleft;
+    public float timeStart;
+    public float happynessStart;
+    public float happynessThreshhol;
+    float happyness;
+
+	// Use this for initialization
+	void Start () {
+        happyness = happynessStart;
+        timeleft = timeStart;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeleft -= Time.deltaTime;
+
+
+		
+	}
+
+    private void updateHappyness(float adjustment)
+    {
+        happyness += adjustment;
+
+    }
+
+
+}
